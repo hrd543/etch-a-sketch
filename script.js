7a0654907db3f0fb432376903ceac59ab82b6d07
Henry Dearn <dearn.henry@gmail.com>
2021-05-07 16:13:52 +0100

    Add clear and grid size buttons

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 const container = document.querySelector('.container');
-let numRows = 32;
-container.style.setProperty('--num-rows', numRows);
-for (let i=1; i<=numRows*numRows; i++){
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('box', 'clear');
-    container.appendChild(newDiv);
+createGrid(20);
+
+let clearButton = document.querySelector('.clear-btn');
+let changeButton = document.querySelector('.change');
+clearButton.addEventListener("click", resetBoard);
+changeButton.addEventListener("click", changeGridSize);
+
+
+function changeGridSize() {
+    while (true) {
+        let nRowsInput = prompt("Enter the number of rows between 1 and 100: ");
+        nRows = parseInt(nRowsInput);
+        if (nRowsInput == null) {
+            return true;
+        }
+        if (nRows < 1 || nRows > 100 || isNaN(nRows)) {
+            alert("Please enter a number between 1 and 100!");
+            continue
+        }
+        console.log(nRows);
+        createGrid(nRows);
+        return true;
+    }
+}
+
+function createGrid(numRows) {
+    container.innerHTML = '';
+    container.style.setProperty('--num-rows', numRows);
+    for (let i = 1; i <= numRows * numRows; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('box', 'clear');
+        newDiv.addEventListener("mouseover", changeColour);
+        container.appendChild(newDiv);
+    }
+}
+
+function resetBoard() {
+    container.querySelectorAll('div').forEach(element => {
+        element.classList.add("clear");
+    });
+}
+
+function changeColour(event) {
+    this.classList.remove("clear");
 }
